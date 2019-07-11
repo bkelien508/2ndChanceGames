b84d203b897d9536d05f425377eab2d68167caa9
Benjamin Kelien <bkelien508@gmail.com>
2019-07-11 19:26:39 -0400

    changed the name from apps.js to scripts.js | Added a mouseover function

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var games = {
+    name: 'SuperMarioWorld',
+    developer: 'Nintendo',
+    released: '1990'
+};
+
+// Changes the color when the mouse goes over the title
+$(".card-title").mouseover(function(){
+    $(".card-title").css("background-color", "aquamarine");
+  });
