a39c421cda2753af018273190c3850620ce52389
Benjamin Kelien <bkelien508@gmail.com>
2019-07-11 19:29:55 -0400

    renamed JS file

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var games = {
-    name: 'SuperMarioWorld',
-    developer: 'Nintendo',
-    released: '1990'
-};
-
-$(window).scroll(function() {
-    $('header').css('top', $(this).scrollTop() + "px");
-});
