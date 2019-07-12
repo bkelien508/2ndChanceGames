5323c35bf9b7dec7dd1a2e0b302e258c14f4d190
Benjamin Kelien <bkelien508@gmail.com>
2019-07-11 22:04:04 -0400

    added Event Listener for mouseover of game-title

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 var games = {
-    name: 'SuperMarioWorld',
-    developer: 'Nintendo',
-    released: '1990'
+  name: 'SuperMarioWorld',
+  developer: 'Nintendo',
+  released: '1990'
 };
 
 // Changes the color when the mouse goes over the title
-$(".card-title").mouseover(function(){
-    $(".card-title").css("background-color", "aquamarine");
-  });
+// $(document).ready(function () {
+//   $(".card-title").mousemove(function () {
+//     $(".card-title").css("background-color", "yellow");
+//   });
+//   $(".card-title").mouseout(function () {
+//     $(".card-title").css("background-color", "red");
+//   });
+// });
+
+document.getElementById("game-title").addEventListener("mouseover", mouseOver);
+document.getElementById("game-title").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  document.getElementById("game-title").style.color = "blue";
+}
+
+function mouseOut() {
+  document.getElementById("game-title").style.color = "lightgrey";
+}
