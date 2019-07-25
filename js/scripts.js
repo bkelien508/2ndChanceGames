6d58229d8857cf358ce108e2132e3591d4f4907b
Benjamin Kelien <bkelien508@gmail.com>
2019-07-25 02:48:07 -0400

    started building filtering array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-const systems = document.querySelector('data-category');
+var games = [
+    {name: "Super Mario World", system: "superNintendo"},
+    {name: "Super Metroid", system: "superNintendo"},
+    {name: "Super Mario RPG", system: "superNintendo"},
+    {name: "Castlevania: Circle of the Moon", system: "gameBoy"},
+    {name: "Fire Emblem", system: "gameBoy"},
+    {name: "Golden Sun", system: "gameBoy"},
+    {name: "Halo: Combat Evolved", system: "xbox"},
+    {name: "Fable", system: "xbox"},
+    {name: "Clash of Clans", system: "mobile"},
+    {name: "Betrayal At House On The Haunted Hill", system: "boardGames"}
+
+
+var marvelHeroes =  heroes.filter(function(hero) {
+        return hero.franchise == “Marvel”;
+    });
+
+    {id: superNintendo}, 
+    {id: gameBoy}, 
+    {id: xbox}, 
+    {id: mobile}, 
+    {id: boardGames}];
