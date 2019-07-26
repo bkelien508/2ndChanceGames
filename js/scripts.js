65953ce7906e9ecf8ec9b5f40c8f28ea2d395732
Benjamin Kelien <bkelien508@gmail.com>
2019-07-26 11:00:58 -0400

    modified comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 // An array I built but didn't have time to map it properly.  To be continued after class ends...
 
-let games = [
-    {name: "Super Mario World", system: "superNintendo"},
-    {name: "Super Metroid", system: "superNintendo"},
-    {name: "Super Mario RPG", system: "superNintendo"},
-    {name: "Castlevania: Circle of the Moon", system: "gameBoy"},
-    {name: "Fire Emblem", system: "gameBoy"},
-    {name: "Golden Sun", system: "gameBoy"},
-    {name: "Halo: Combat Evolved", system: "xbox"},
-    {name: "Fable", system: "xbox"},
-    {name: "Clash of Clans", system: "mobile"},
-    {name: "Betrayal At House On The Haunted Hill", system: "boardGames"}];
+// let games = [
+//   { name: "Super Mario World", system: "superNintendo" },
+//   { name: "Super Metroid", system: "superNintendo" },
+//   { name: "Super Mario RPG", system: "superNintendo" },
+//   { name: "Castlevania: Circle of the Moon", system: "gameBoy" },
+//   { name: "Fire Emblem", system: "gameBoy" },
+//   { name: "Golden Sun", system: "gameBoy" },
+//   { name: "Halo: Combat Evolved", system: "xbox" },
+//   { name: "Fable", system: "xbox" },
+//   { name: "Clash of Clans", system: "mobile" },
+//   { name: "Betrayal At House On The Haunted Hill", system: "boardGames" }];
 
 
 // Filters the systems but doesn't filter the cards correctly
@@ -54,6 +54,8 @@ let games = [
   var filter = document.getElementById("filter");
   filter.addEventListener("keyup", filterOptionsHandler, false);
 }());
+
+
 
 // hightlights the class card-title so it is easier to read the title of the game
 $(document).ready(function () {
