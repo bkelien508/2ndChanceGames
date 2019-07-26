ca89dbe2822dcbac08ebd20498c7861767f56718
Benjamin Kelien <bkelien508@gmail.com>
2019-07-26 10:39:54 -0400

    added filtering JS

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,55 @@
 //     {name: "Clash of Clans", system: "mobile"},
 //     {name: "Betrayal At House On The Haunted Hill", system: "boardGames"}];
 
-    //hightlights the class card-title so it is easier to read the title of the game
-    $(document).ready(function(){
-        $(".card-title").mouseover(function(){
-          $(".card-title").css("background-color", "lightblue");
-        });
-        $(".card-title").mouseleave(function(){
-          $(".card-title").css("background-color", "red");
-        });
-      });
+
+// Filters the systems but doesn't filter the cards correctly
+(function () {
+  var optionsCache = [];
+
+  function optionsArray(select) {
+    var reduce = Array.prototype.reduce;
+    return reduce.call(select.options, function addToCache(options, option) {
+      options.push(option);
+      return options;
+    }, []);
+  }
+  function filterOptions(filterValue, optionsCache) {
+    return optionsCache.reduce(function filterCache(options, option) {
+      var optionText = option.textContent;
+      if (option.text.toLowerCase().match(filterValue.toLowerCase())) {
+        options.push(option);
+      }
+      return options;
+    }, []);
+  }
+  function replaceOptions(select, options) {
+    while (select.options.length > 0) {
+      select.remove(0);
+    }
+    options.forEach(function addOption(option) {
+      select.add(option);
+    });
+  }
+  function filterOptionsHandler(evt) {
+    var filterField = evt.target;
+    var targetSelect = document.getElementById("system");
+    if (optionsCache.length < 1) {
+      optionsCache = optionsArray(targetSelect);
+    }
+    var options = filterOptions(filterField.value, optionsCache);
+    replaceOptions(targetSelect, options);
+  }
+  // attach filter event to trigger on keyup
+  var filter = document.getElementById("filter");
+  filter.addEventListener("keyup", filterOptionsHandler, false);
+}());
+
+// hightlights the class card-title so it is easier to read the title of the game
+$(document).ready(function () {
+  $(".card-title").mouseover(function () {
+    $(".card-title").css("background-color", "lightblue");
+  });
+  $(".card-title").mouseleave(function () {
+    $(".card-title").css("background-color", "red");
+  });
+});
