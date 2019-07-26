b35b1227d35e95d557bf23aa70d7c2452fcc8433
Benjamin Kelien <bkelien508@gmail.com>
2019-07-26 10:43:48 -0400

    modified filter | still only processes systems and not games

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 // An array I built but didn't have time to map it properly.  To be continued after class ends...
 
-// const games = [
-//     {name: "Super Mario World", system: "superNintendo"},
-//     {name: "Super Metroid", system: "superNintendo"},
-//     {name: "Super Mario RPG", system: "superNintendo"},
-//     {name: "Castlevania: Circle of the Moon", system: "gameBoy"},
-//     {name: "Fire Emblem", system: "gameBoy"},
-//     {name: "Golden Sun", system: "gameBoy"},
-//     {name: "Halo: Combat Evolved", system: "xbox"},
-//     {name: "Fable", system: "xbox"},
-//     {name: "Clash of Clans", system: "mobile"},
-//     {name: "Betrayal At House On The Haunted Hill", system: "boardGames"}];
+const games = [
+    {name: "Super Mario World", system: "superNintendo"},
+    {name: "Super Metroid", system: "superNintendo"},
+    {name: "Super Mario RPG", system: "superNintendo"},
+    {name: "Castlevania: Circle of the Moon", system: "gameBoy"},
+    {name: "Fire Emblem", system: "gameBoy"},
+    {name: "Golden Sun", system: "gameBoy"},
+    {name: "Halo: Combat Evolved", system: "xbox"},
+    {name: "Fable", system: "xbox"},
+    {name: "Clash of Clans", system: "mobile"},
+    {name: "Betrayal At House On The Haunted Hill", system: "boardGames"}];
 
 
 // Filters the systems but doesn't filter the cards correctly
