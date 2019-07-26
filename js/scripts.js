c0e46ce850a401f3cb5e329f1ed7ee01b905c438
Benjamin Kelien <bkelien508@gmail.com>
2019-07-25 21:07:33 -0400

    added mouseover jQuery function to card-title class

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
-var games = [
-    {name: "Super Mario World", system: "superNintendo"},
-    {name: "Super Metroid", system: "superNintendo"},
-    {name: "Super Mario RPG", system: "superNintendo"},
-    {name: "Castlevania: Circle of the Moon", system: "gameBoy"},
-    {name: "Fire Emblem", system: "gameBoy"},
-    {name: "Golden Sun", system: "gameBoy"},
-    {name: "Halo: Combat Evolved", system: "xbox"},
-    {name: "Fable", system: "xbox"},
-    {name: "Clash of Clans", system: "mobile"},
-    {name: "Betrayal At House On The Haunted Hill", system: "boardGames"}];
+// An array I built but didn't have time to map it properly.  To be continued after class ends...
 
+// const games = [
+//     {name: "Super Mario World", system: "superNintendo"},
+//     {name: "Super Metroid", system: "superNintendo"},
+//     {name: "Super Mario RPG", system: "superNintendo"},
+//     {name: "Castlevania: Circle of the Moon", system: "gameBoy"},
+//     {name: "Fire Emblem", system: "gameBoy"},
+//     {name: "Golden Sun", system: "gameBoy"},
+//     {name: "Halo: Combat Evolved", system: "xbox"},
+//     {name: "Fable", system: "xbox"},
+//     {name: "Clash of Clans", system: "mobile"},
+//     {name: "Betrayal At House On The Haunted Hill", system: "boardGames"}];
 
-var marvelHeroes =  heroes.filter(function(hero) {
-        return hero.franchise == “Marvel”;
-    });
-
-    {id: superNintendo}, 
-    {id: gameBoy}, 
-    {id: xbox}, 
-    {id: mobile}, 
-    {id: boardGames}];
+    //hightlights the class card-title so it is easier to read the title of the game
+    $(document).ready(function(){
+        $(".card-title").mouseover(function(){
+          $(".card-title").css("background-color", "lightblue");
+        });
+        $(".card-title").mouseleave(function(){
+          $(".card-title").css("background-color", "red");
+        });
+      });
